d1d73695e0bd97ad83d327efbf22a68ea98e2fa3
Titrat Cristina <titratcristina@gmail.com>
2020-04-04 14:27:10 +0300

    Update condition

--- resurse/javascript/script.js
+++ resurse/javascript/script.js
@@ -4,7 +4,7 @@ function randomIntFromInterval(min, max) { // min and max included
 
 var currently = new Date()
 
-if (parseInt(currently.toTimeString().split(" ")[0].split(":")[0]) >= 13 && parseInt(currently.toTimeString().split(" ")[0].split(":")[0]) <= 15) {
+if (parseInt(currently.toTimeString().split(" ")[0].split(":")[0]) >= 13 && parseInt(currently.toTimeString().split(" ")[0].split(":")[0]) <= 16) {
     document.getElementById("code").innerHTML = randomIntFromInterval(10, 50);
     document.getElementById("promocode").style = "display: block !important;";
 }
